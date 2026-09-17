955714602e848bdf58255d0cb750116db17cbbf1
Tiago <tiago.xavier@prof.infnet.edu.br>
2026-09-17 11:50:08 -0300

    feat: add middleware to measure and return request processing time

--- etapa_3_4/main.py
+++ etapa_3_4/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+import time
+
+from fastapi import FastAPI, Request
 from sqlmodel import SQLModel
 
 from database import engine
@@ -6,6 +8,16 @@ from routes.auth_route import router as auth_router
 from routes.prediction_route import router as prediction_router
 
 app = FastAPI()
+
+@app.middleware("http")
+async def process_time(request: Request, call_next):
+    start = time.time()
+    response = await call_next(request)
+    final = time.time()
+    response.headers["X-Process-Time"] = str(final - start)
+    return response
+
+
 
 SQLModel.metadata.create_all(engine)
 
