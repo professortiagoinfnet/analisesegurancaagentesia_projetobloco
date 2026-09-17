528d940c04e7c0a15ab19a5bf6805b8eb0c4c619
Tiago <tiago.xavier@prof.infnet.edu.br>
2026-09-17 11:49:43 -0300

    feat: add rate limiting and validation endpoints for predictions

--- etapa_3_4/routes/prediction_route.py
+++ etapa_3_4/routes/prediction_route.py
@@ -1,3 +1,12 @@
+from fastapi import Request
+from slowapi import Limiter
+from slowapi.util import get_remote_address
+
+from models.prediction_request_model import PredictionRequest
+from models.prediction_response_model import PredictionResponse, PredictionValid
+from fastapi import Response
+import time
+
 from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session, select
 
@@ -7,7 +16,7 @@ from models.user_sql_model import User
 from security.dependencies import get_current_user
 
 router = APIRouter()
-
+limiter = Limiter(key_func=get_remote_address)
 
 @router.get("/predictions/{prediction_id}")
 def get_prediction(
@@ -27,6 +36,26 @@ def get_prediction(
 
     return prediction
 
+@router.get("/predicitons_limited")
+@limiter.limit("5/minute")
+def get_all_predicitons(
+    request: Request,
+    session: Session = Depends(get_session),
+    current_user: User = Depends(get_current_user)):
+    if current_user.id != 1:
+        raise HTTPException(status_code=404, detail="Prediction não encontrada")
+    
+    statement = select(Prediction).where(
+        Prediction.owner_id > 0
+    )
+
+    predictions = session.exec(statement).fetchall()
+
+    if len(predictions) == 0:
+        raise HTTPException(status_code=404, detail="Prediction não encontrada")
+    
+    return predictions
+
 @router.get("/predicitons")
 def get_all_predicitons(
     session: Session = Depends(get_session),
@@ -44,3 +73,21 @@ def get_all_predicitons(
         raise HTTPException(status_code=404, detail="Prediction não encontrada")
     
     return predictions
+
+@router.post("/predict")
+def predict (predictionRequest: PredictionRequest, response: Response):
+    predictionResponse = PredictionResponse(
+        message=predictionRequest.message,
+        intent='cancelamento'
+    )
+    return predictionResponse
+
+@router.post("/valid")
+def valid(predictionRequest: PredictionRequest, response: Response):
+    predictionResponse = PredictionValid(
+        message=predictionRequest.message,
+        valid='Válido' if len(predictionRequest.message) > 5 else "Inválido"
+    )
+    return predictionResponse
+
+                               
